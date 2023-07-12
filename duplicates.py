8e9fac0a0642ac343e1b83d1a722aed60bef7caa
yd2506 <76481952+yd776@users.noreply.github.com>
2023-07-12 09:33:26 +0530

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -10,4 +10,22 @@ class Solution:
                 counts[i]=1
             
         return -1
+
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def findOccurences(self, A):
+        
+        A.sort()
+        counts=dict()
+        for i in A:
+            if i in counts:
+                counts[i]+=1
+            else:
+                counts[i] =1
+        my_list = list(counts.values())
+        return my_list
+        
+        
         
