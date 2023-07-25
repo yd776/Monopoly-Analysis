114ec67517f6c91b11cd0b107ec018b1f875b556
yd2506 <76481952+yd776@users.noreply.github.com>
2023-07-25 12:34:32 +0530

    Create backtracking.py

---
+++ backtracking.py
@@ -0,0 +1,18 @@
+def getPermutation(self, n, k):
+        fact = 1
+        numbers = []
+        for i in range(1, n):
+            fact *= i
+            numbers.append(i)
+        numbers.append(n)
+        ans = ""
+        k -= 1
+        while True:
+            ans += str(numbers[k // fact])
+            numbers.pop(k // fact)
+            if not numbers:
+                break
+            k %= fact
+            fact //= len(numbers)
+        return ans
+
